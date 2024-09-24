9029db8dbc4a9b768b850c6326449760e38b99e7
migu31-h <miguel196915.maha@gmail.com>
2024-09-23 23:44:28 -0400

    added name and link to github

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><Link to="https://github.com/migu31-h/kanbas-react-web-app.git" id="wd-github">kanbas-react-web-app</Link></li>
       </ul>
     );
   }
