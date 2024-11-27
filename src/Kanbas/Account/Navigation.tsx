db2927b63023c322ab61f465f0a65a3bdede84af
migu31-h <miguel196915.maha@gmail.com>
2024-11-27 11:38:27 -0500

    Connecting from Node.js

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -4,6 +4,8 @@ export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+
   return (
     <div id="wd-account-navigation">
       <ul style={{ listStyle: "none" }}>
@@ -24,6 +26,10 @@ export default function AccountNavigation() {
           <Link className="text-danger decoration-none  wd-account-navigation-not-selec"
             style={{ paddingLeft: "8px" }}
             to={`/Kanbas/Account/Profile`} > Profile </Link>
+        </li>
+        <li>
+          {currentUser && currentUser.role === "ADMIN" && (
+            <Link style={{ paddingLeft: "8px", paddingRight: "8px" }} to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link>)}
         </li>
       </ul>
     </div>
